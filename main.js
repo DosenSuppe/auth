28071dd704b878d3cd0e233627b298f88e2c1831
DosenSuppe2_0 <97290053+DosenSuppe@users.noreply.github.com>
2023-11-25 14:45:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const fileReader = require('fs');   //Import the file reader
 const database = require("nedb");   //Import the datastore
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
-const XMLHttpRequest = require('xhr2');
+
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
