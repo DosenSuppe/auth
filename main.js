fcbf852276aba26961ef2bfd3c1fadc09ee33116
DosenSuppe2_0 <97290053+DosenSuppe@users.noreply.github.com>
2023-11-25 12:13:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,32 +4,57 @@ const database = require("nedb");   //Import the datastore
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
 
+const authDatastore = new database({filename: "./db.txt", autoload: true});
+
+function verifyHash(inputString) {
+    return (inputString == undefined)? null : inputString;
+}
+
+function verify(inputString) {
+    let hash = verifyHash(inputString);
+    return (hash != null)? true : false;
+}
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
 
-    let source = req.query.source;
-    let sourceType = req.query.sourceType;
+    let registerNew = req.query.regNew || false;
+    let gameId = req.query.gameId;
+    let authKey = req.query.authKey;
 
-    if (sourceType == "name") {
-        console.log("sourceType = name");
+    let dump = req.query.dump || false;
 
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://groups.roblox.com/v1/groups/search?keyword=Arsenal%20Pro%20&prioritizeExactMatch=true&limit=10");
-        xhr.send();
-        xhr.responseType = "json";
-        xhr.onload = () => {
-          if (xhr.readyState == 4 && xhr.status == 200) {
-            const data = xhr.response;
-            console.log(data);
-          } else {
-            console.log(`Error: ${xhr.status}`);
-          }
-        };
-    } 
+    if (dump) {
+        console.log("dumping Data of datastore");
+    }
+
+    if (registerNew) {
+        if (verify(authKey)) {
+            authDatastore.insert({key: authKey, id: null}, function(err, newDoc) { console.log("newDoc:", newDoc); });
+            res.send("<a>Created new key!</a>");
+        } else res.sendStatus(403);
+                   
+    } else {
+        authDatastore.findOne({key: verifyHash(authKey)}, function(err, doc) {
+            if (doc != null) {
+                if (doc.id == null) {
+                    // Set an existing field's value
+                    authDatastore.update({ key: verifyHash(authKey) }, { $set: { id: gameId } }, { multi: true }, function (err, numReplaced) { console.log("Replaced:", numReplaced, "entities!"); });
+                    res.sendStatus(200);
+                    
+                } else {
+                    console.log("FROM DB: ", doc.id, doc.key, gameId, verifyHash(authKey));
+                    if (doc.id == gameId && doc.key == verifyHash(authKey)) {
+                        res.sendStatus(200);
+                    } else res.sendStatus(403);
+                }
+            } else res.sendStatus(403);
+        });
+    }
+
+
 });
 
-app.listen(port, "0.0.0.0", () => {            //server starts listening for any attempts from a client to connect at port: {port}
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
 });
-
